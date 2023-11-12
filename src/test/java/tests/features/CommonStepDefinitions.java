ae12ff8c6aea446ae79f77008797baf9cd921d55
abhijeetpraveen <abhijeetpraveen@gmail.com>
2023-11-11 20:22:30 -0500

    Implemented testing service is available

--- src/test/java/tests/features/CommonStepDefinitions.java
+++ src/test/java/tests/features/CommonStepDefinitions.java
@@ -1,13 +1,25 @@
 package tests.features;
 
+import helpers.ApiHelper;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 public class CommonStepDefinitions {
     @Given("the API server is running and available")
-    public void theAPIServerIsRunningAndAvailable() {
-        // todo
+    public void theAPIServerIsRunningAndAvailable() throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpResponse response = ApiHelper.sendHttpRequest("get", "http://localhost:4567/docs", null, httpClient);
+        int statusCode = response.getStatusLine().getStatusCode();
+        assertEquals(HttpStatus.SC_OK, statusCode);
     }
 
     @Given("the following projects exist in the system:")
