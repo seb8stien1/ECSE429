3c689f33ad21081f239699b052c4d91482938e85
abhijeetpraveen <abhijeetpraveen@gmail.com>
2023-11-12 23:02:59 -0500

    finished delete todo

--- src/test/java/tests/features/DeleteTodo.java
+++ src/test/java/tests/features/DeleteTodo.java
@@ -2,21 +2,78 @@ package tests.features;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import lombok.AllArgsConstructor;
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import response.Todo;
+import response.TodoResponse;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+import static helpers.ApiHelper.deserialize;
+import static helpers.TodoHelper.deleteTodo;
+import static helpers.TodoHelper.getAllTodos;
+
+@AllArgsConstructor
 public class DeleteTodo {
 
+    private final TestContext testContext;
+
     @When("a user attempts to delete the todo with the title {string}")
-    public void aUserAttemptsToDeleteTheTodoWithTheTitle(String todoTitle) {
-        // todo
+    public void aUserAttemptsToDeleteTheTodoWithTheTitle(String todoTitle) throws IOException {
+        HashMap<String, Todo> createdTodos = testContext.get("createdTodos", HashMap.class);
+        CloseableHttpClient httpClient = testContext.get("httpClient", CloseableHttpClient.class);
+
+        String todoID = createdTodos.get(todoTitle).getId();
+
+        deleteTodo(todoID, httpClient);
     }
 
     @Then("the todo with the title {string} shall be removed from the system")
-    public void theTodoWithTheTitleShallBeRemovedFromTheSystem(String todoTitle) {
-        // todo
+    public void theTodoWithTheTitleShallBeRemovedFromTheSystem(String todoTitle) throws IOException {
+        HashMap<String, Todo> createdTodos = testContext.get("createdTodos", HashMap.class);
+        CloseableHttpClient httpClient = testContext.get("httpClient", CloseableHttpClient.class);
+
+        String todoID = createdTodos.get(todoTitle).getId();
+
+        HttpResponse response = getAllTodos(httpClient);
+        TodoResponse todoResponse = deserialize(response, TodoResponse.class);
+
+        Optional<Todo> todoOptional = todoResponse.getTodos()
+                .stream()
+                .filter(todo -> todoID.equals(todo.getId()))
+                .findFirst();
+        assertFalse(todoOptional.isPresent());
+    }
+
+    @When("a user attempts to delete the already deleted todo with the title {string}")
+    public void aUserAttemptsToDeleteTheAlreadyDeletedTodoWithTheTitle(String todoTitle) throws IOException {
+        HashMap<String, Todo> createdTodos = testContext.get("createdTodos", HashMap.class);
+        CloseableHttpClient httpClient = testContext.get("httpClient", CloseableHttpClient.class);
+
+        Todo nonExistentTodo = createdTodos.get(todoTitle);
+        assertNull(nonExistentTodo);
+        String nonExistentTodoID = UUID.randomUUID().toString();
+
+        HttpResponse response = deleteTodo(nonExistentTodoID, httpClient);
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        testContext.set("statusCode", statusCode);
+
     }
 
     @When("a user attempts to delete the todo with an invalid ID {string}")
-    public void aUserAttemptsToDeleteTheTodoWithAnInvalidID(String todoID) {
-        // todo
+    public void aUserAttemptsToDeleteTheTodoWithAnInvalidID(String invalidID) throws IOException {
+        CloseableHttpClient httpClient = testContext.get("httpClient", CloseableHttpClient.class);
+
+        HttpResponse response = deleteTodo(invalidID, httpClient);
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        testContext.set("statusCode", statusCode);
     }
 }
--- src/test/resources/DeleteTodo.feature
+++ src/test/resources/DeleteTodo.feature
@@ -18,16 +18,16 @@ Feature: Delete Todo
     Examples:
       | todoTitle       | expectedTodoCount |
       | Clean House     | 2                 |
-      | Call Abhigyan   | 1                 |
+      | Call Abhigyan   | 2                 |
 
   Scenario Outline: Alternate Flow - A user attempts to delete a todo that has already been deleted
-    When a user attempts to delete the todo with the title "<todoTitle>"
+    When a user attempts to delete the already deleted todo with the title "<todoTitle>"
     Then the status code returned by the API is "<statusCode>"
 
     Examples:
-      | todoTitle   | statusCode |
-      | Clean House | 404        |
-      | Call John   | 404        |
+      | todoTitle    | statusCode |
+      | Clean Office | 404        |
+      | Call Seb     | 404        |
 
   Scenario Outline: Error Flow - A user attempts to delete a todo with an invalid ID
     When a user attempts to delete the todo with an invalid ID "<todoID>"
@@ -35,8 +35,8 @@ Feature: Delete Todo
 
     Examples:
       | todoID | statusCode |
-      | abc123 | 400        |
-      | 99999  | 400        |
+      | abc123 | 404        |
+      | 99999  | 404        |
 
   Scenario: Teardown
     Then the system is restored to the original state
